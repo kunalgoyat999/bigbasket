10c1094067f6054e636b77b702d66cdd20bab9da
Kunal <kunalgoyat999@gmail.com>
2023-02-10 16:32:20 +0530

    added payment page

--- Payment Page/payment.js
+++ Payment Page/payment.js
@@ -1,6 +1,57 @@
-// adding click event on pay button
+displayamount();
+
+function displayamount() {
+  var basketvalue = document.getElementById("basketvalue");
+  var total = document.getElementById("tot");
+  basketvalue.textContent = 50;
+
+  total.textContent = +50;
+}
+
+var couponInput = document.getElementById("coupon-input");
+var applyBtn = document.getElementById("apply-btn");
+var message = document.getElementById("message");
+
+applyBtn.addEventListener("click", function () {
+  const couponCode = couponInput.value;
+  var basketvalue = document.getElementById("basketvalue");
+  var total = document.getElementById("tot");
+  if (couponCode === "masai30") {
+    basketvalue.textContent -= Number(basketvalue.textContent) * (3 / 10);
+    total.textContent -= Number(total.textContent) * (3 / 10);
+  } else {
+    message.innerHTML = "Invalid coupon code. Please try again.";
+  }
+});
+
+// Save Address
+
+document.getElementById("submitbutton").addEventListener("click", saveAddress)
+
+var addArr = JSON.parse(localStorage.getItem("address")) || [];
+
+function saveAddress() {
+  var fullname = document.getElementById("fullName").value;
+  var street = document.getElementById("address").value;
+  var city = document.getElementById("city").value;
+  var state = document.getElementById("state").value;
+  var zip = document.getElementById("zip").value;
+
+  console.log(street + " " + city+ " "+state+" ,"+ zip);
+
+  var addobj = {
+    name: fullname,
+    address: street + " ," + city+ " ,"+state+" ,"+ zip
+  }
+
+  addArr.push(addobj)
+
+  localStorage.setItem("address", JSON.stringify(addArr))
+
+}
+
+// For Payment
 document.getElementById("pay").addEventListener("click", pay);
-// creating the function 
 function pay() {
   let cardNo = document.getElementById("card_no").value;
   let v = document.getElementById("valid").value;
@@ -16,20 +67,6 @@ function pay() {
     alert("Invalid cvv");
   } else {
     alert("PAYMENT SUCCESSFULL AND YOUR ORDER IS PLACED");
-    window.location.href = "index.html";
+    window.location.href = "../../index.html";
   }
 }
-
-
-const couponInput = document.getElementById("coupon-input");
-const applyBtn = document.getElementById("apply-btn");
-const message = document.getElementById("message");
-
-applyBtn.addEventListener("click", function () {
-  const couponCode = couponInput.value;
-  if (couponCode === "DISCOUNT") {
-    message.innerHTML = "Coupon code applied. Discount added to the total.";
-  } else {
-    message.innerHTML = "Invalid coupon code. Please try again.";
-  }
-});
